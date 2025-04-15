717b2f9a9c93b9e61f6800c263dcbcec02d1aa10
Mirelle Maria <mirelle.maria.oliveira.rocha@ccc.ufcg.edu.br>
2025-04-15 13:47:58 -0300

    resolve Rotações em Árvores Binárias de Pesquisa

--- tst/Rotacoes.java
+++ tst/Rotacoes.java
@@ -1,19 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class Rotacoes {
+class Rotacoes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] entrada = sc.nextLine().split(" ");
 
+        AVL avl = new AVL();
+        for (String s : entrada)
+            avl.add(Integer.parseInt(s));
 
+        if (!avl.rotacionou) System.out.println("balanceada");
     }
 
     public static class AVL {
         private Node root;
         private int size;
+        private boolean rotacionou;
 
         public AVL() {
             this.size = -1;
+            rotacionou = false;
         }
 
         public boolean isEmpty() {
@@ -31,9 +39,7 @@ public class Rotacoes {
                             Node newNode = new Node(element);
                             aux.left = newNode;
                             newNode.parent = aux;
-
-                            Node desbalanceado = checkBalance(newNode);
-                            if (desbalanceado != null) callBestRotation(desbalanceado);
+                            rebalance(newNode);
                             return;
                         }
                         aux = aux.left;
@@ -42,9 +48,7 @@ public class Rotacoes {
                             Node newNode = new Node(element);
                             aux.right = newNode;
                             newNode.parent = aux;
-
-                            Node desbalanceado = checkBalance(newNode);
-                            if (desbalanceado != null) callBestRotation(desbalanceado);
+                            rebalance(newNode);
                             return;
                         }
                         aux = aux.right;
@@ -53,18 +57,92 @@ public class Rotacoes {
             }
         }
 
+        private void rebalance(Node node) {
+            Node desbalanceado = checkBalance(node);
+            if (desbalanceado != null) {
+                callBestRotation(desbalanceado);
+            }
+        }
+
         public Node checkBalance(Node node) {
             Node aux = node;
             while (aux != null) {
-                if (!aux.isBalanced()) {
-                    return aux;
-                } else if (aux.parent != null) {
-                    aux = aux.parent;
-                } else {
-                    break;
-                }
+                if (!aux.isBalanced()) return aux;
+                else if (aux.parent != null) aux = aux.parent;
+                else break;
             }
             return null;
+        }
+
+        public void callBestRotation(Node desbalanceado) {
+            Node x = desbalanceado;
+            if (x.isLeftPending()) {
+                Node y = x.left;
+                if (y.left != null) rotateRight(x);
+                else {
+                    rotateLeft(y);
+                    rotateRight(x);
+                }
+            } else {
+                Node y = x.right;
+                if (y.right != null) rotateLeft(x);
+                else {
+                    rotateRight(y);
+                    rotateLeft(x);
+                }
+            }
+
+            rotacionou = true;
+        }
+
+        public void rotateLeft(Node node) {
+            System.out.println("rot_esq(" + node.value + ")");
+            Node newRoot = node.right;
+            newRoot.parent = node.parent;
+
+            node.right = newRoot.left;
+            newRoot.left = node;
+
+            node.parent = newRoot;
+
+            if (newRoot.parent != null) {
+                if (newRoot.parent.right == node) newRoot.parent.right = newRoot;
+                else newRoot.parent.left = newRoot;
+            } else root = newRoot;
+
+            preOrder();
+        }
+
+        public void rotateRight(Node node) {
+            System.out.println("rot_dir(" + node.value + ")");
+            Node newRoot = node.left;
+            newRoot.parent = node.parent;
+
+            node.left = newRoot.right;
+            newRoot.right = node;
+
+            node.parent = newRoot;
+
+            if (newRoot.parent != null) {
+                if (newRoot.parent.left == node) newRoot.parent.left = newRoot;
+                else newRoot.parent.right = newRoot;
+            } else root = newRoot;
+
+            preOrder();
+        }
+
+        public void preOrder() {
+            List<Integer> result = new ArrayList<>();
+            preOrder(this.root, result);
+            System.out.println(result);
+        }
+
+        private void preOrder(Node node, List<Integer> result) {
+            if (node != null) {
+                result.add(node.value);
+                preOrder(node.left, result);
+                preOrder(node.right, result);
+            }
         }
     }
 
@@ -75,7 +153,26 @@ public class Rotacoes {
         Node parent;
 
         public Node(int v) {
-            this.value = v;
+            value = v;
+        }
+
+        public int height() {
+            if (left == null && right == null) return 0;
+            else if (left == null) return 1 + right.height();
+            else if (right == null) return 1 + left.height();
+            else return 1 + Math.max(left.height(), right.height());
+        }
+
+        public boolean isBalanced() {
+            int left = this.left == null ? -1 : this.left.height();
+            int right = this.right == null ? -1 : this.right.height();
+            return Math.abs(left - right) <= 1;
+        }
+
+        public boolean isLeftPending() {
+            int left = this.left == null ? -1 : this.left.height();
+            int right = this.right == null ? -1 : this.right.height();
+            return left - right >= 1;
         }
     }
 }
